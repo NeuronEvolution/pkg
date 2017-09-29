ac0aa373de0ce555991f651ca310c7d3d93de1a5
god <164798848@qq.com>
2017-09-29 14:43:17 +0800

    clean

---
+++ filesystem.go
@@ -0,0 +1,53 @@
+package pkg
+
+import (
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func MakeDirIfNotExists(filePath string) (err error) {
+	dir := filepath.Dir(filePath)
+	dirInfo, err := os.Stat(dir)
+	if dirInfo == nil || os.IsNotExist(err) {
+		logrus.WithField("_func_", "MakeDirIfNotExists").WithField("filePath", filePath).Infoln()
+		err = os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			return errors.Wrap(err, "MakeDirIfNotExists failed "+filePath)
+		}
+	}
+
+	return nil
+}
+
+func NewFile(filePath string, data []byte) (err error) {
+	logrus.WithField("_func_", "NewFile").WithField("filePath", filePath).Infoln()
+	err = MakeDirIfNotExists(filePath)
+	if err != nil {
+		return errors.Wrap(err, "NewFile failed "+filePath)
+	}
+
+	err = ioutil.WriteFile(filePath, data, os.ModePerm|os.ModeExclusive)
+	if err != nil {
+		return errors.Wrap(err, "NewFile failed "+filePath)
+	}
+
+	return nil
+}
+
+func AppendFile(filePath string, data []byte) (err error) {
+	logrus.WithField("_func_", "AppendFile").WithField("filePath", filePath).Infoln()
+	err = MakeDirIfNotExists(filePath)
+	if err != nil {
+		return errors.Wrap(err, "AppendFile failed "+filePath)
+	}
+
+	err = ioutil.WriteFile(filePath, data, os.ModeAppend|os.ModePerm|os.ModeExclusive)
+	if err != nil {
+		return errors.Wrap(err, "AppendFile failed "+filePath)
+	}
+
+	return nil
+}
